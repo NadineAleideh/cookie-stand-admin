487db547606da1ee270fe1b2e1aa033b6645da69
NadineAleideh <nadinealeideh75@gmail.com>
2023-10-05 10:46:40 +0000

    almost done lab37

---
+++ app/Components/Footer.js
@@ -0,0 +1,18 @@
+
+function Footer(){
+
+    return(
+    
+    <footer className="py-4 mt-16 mb-0 text-center bg-orange-400">
+     <p className="text-sm text-stone-600">&copy; {new Date().getFullYear()} Cookie Stand Admin</p>
+    
+    </footer>
+    
+    
+    
+    )
+    
+    }
+    
+    
+    export default Footer;
