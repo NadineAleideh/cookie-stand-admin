2d63965b9064ca72b2bb8e7a982f1175bcb92e02
Nadine  Aleideh <nadinealeideh75@gmail.com>
2023-10-07 22:06:53 +0300

    lab38 done

--- app/Components/Header.js
+++ app/Components/Header.js
@@ -1,13 +1,19 @@
+import React from 'react';
+import Link from 'next/link';
 
 const Header = () => {
-    return (
-        <header className="py-4 bg-orange-400 ">
-        <h1 className="pl-2 text-2xl font-bold text-stone-600">Cookie Stand Admin</h1>
-      </header>
-      
-    );
-  };
-  
-  
-  
-  export default Header;
+  return (
+    <header className="py-4 bg-orange-400">
+      <div className="container flex items-center justify-between mx-auto">
+        <h1 className="text-2xl font-bold text-stone-600">Cookie Stand Admin</h1>
+        <nav className="space-x-4">
+          <Link href="/overview" className="text-stone-600 hover:text-stone-800">
+            Overview
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
